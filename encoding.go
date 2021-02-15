386ad127fcd091d5e404d675beff6637ba767f61
Charlie Jonas <charlie@charliejonas.co.uk>
2021-02-15 19:16:51 +0000

    Use base64 encoding in signatures

--- encoding.go
+++ encoding.go
@@ -1,6 +1,7 @@
 package starsign
 
 import (
+	"encoding/base64"
 	"encoding/gob"
 	"io"
 
@@ -8,17 +9,22 @@ import (
 )
 
 func EncodeSignature(out io.Writer, sig *ssh.Signature) error {
-	enc := gob.NewEncoder(out)
+	b64 := base64.NewEncoder(base64.StdEncoding, out)
+	enc := gob.NewEncoder(b64)
 	err := enc.Encode(sig)
 	if err != nil {
+		return err
+	}
+	if err = b64.Close(); err != nil {
 		return err
 	}
 	return nil
 }
 
 func DecodeSignature(in io.Reader) (*ssh.Signature, error) {
+	b64 := base64.NewDecoder(base64.StdEncoding, in)
 	sig := new(ssh.Signature)
-	dec := gob.NewDecoder(in)
+	dec := gob.NewDecoder(b64)
 	err := dec.Decode(sig)
 	if err != nil {
 		return nil, err
