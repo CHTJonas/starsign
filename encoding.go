5a0d0ebd090b6394f9f7fb66357d932c07315037
Charlie Jonas <charlie@charliejonas.co.uk>
2021-02-16 21:49:49 +0000

    Use custom serialisation format with PEM encoding

--- encoding.go
+++ encoding.go
@@ -1,31 +1,83 @@
 package starsign
 
 import (
-	"encoding/base64"
-	"encoding/gob"
+	"bytes"
+	"encoding/binary"
+	"encoding/pem"
+	"fmt"
 	"io"
+	"io/ioutil"
 )
 
+func serialise(sig *Signature) ([]byte, error) {
+	if len(sig.Hash) != 64 {
+		panic("Unexpected BLAKE2 hash length")
+	}
+	buf := new(bytes.Buffer)
+	_, err := buf.Write(sig.Hash)
+	if err != nil {
+		return nil, err
+	}
+	b := make([]byte, 8)
+	length := len(sig.Sig)
+	binary.LittleEndian.PutUint64(b, uint64(length))
+	_, err = buf.Write(b)
+	if err != nil {
+		return nil, err
+	}
+	_, err = buf.Write(sig.Sig)
+	if err != nil {
+		return nil, err
+	}
+	_, err = buf.WriteString(sig.Type)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func deserialise(data []byte) (*Signature, error) {
+	sig := new(Signature)
+	sig.Hash = data[:64]
+	length := binary.LittleEndian.Uint64(data[64:72])
+	end := 72 + int(length)
+	sig.Sig = data[72:end]
+	sig.Type = string(data[end:])
+	return sig, nil
+}
+
 func EncodeSignature(out io.Writer, sig *Signature) error {
-	b64 := base64.NewEncoder(base64.StdEncoding, out)
-	enc := gob.NewEncoder(b64)
-	err := enc.Encode(sig)
+	data, err := serialise(sig)
 	if err != nil {
 		return err
 	}
-	if err = b64.Close(); err != nil {
+	block := &pem.Block{
+		Type:  "STARSIGN SIGNATURE",
+		Bytes: data,
+	}
+	if err = pem.Encode(out, block); err != nil {
 		return err
 	}
 	return nil
 }
 
-func DecodeSignature(in io.Reader) (*Signature, error) {
-	sig := new(Signature)
-	b64 := base64.NewDecoder(base64.StdEncoding, in)
-	dec := gob.NewDecoder(b64)
-	err := dec.Decode(sig)
+func DecodeSignature(in io.Reader) (sig *Signature, err error) {
+	data, err := ioutil.ReadAll(in)
 	if err != nil {
 		return nil, err
 	}
-	return sig, nil
+	defer func() {
+		if r := recover(); r != nil {
+			sig = nil
+			err = fmt.Errorf("Malformed signature data")
+		}
+	}()
+	block, _ := pem.Decode(data)
+	if block.Type != "STARSIGN SIGNATURE" {
+		return nil, fmt.Errorf("Not a Starsign signature: %s", block.Type)
+	}
+	if len(block.Headers) != 0 {
+		return nil, fmt.Errorf("Signature contains untrusted headers")
+	}
+	return deserialise(block.Bytes)
 }
